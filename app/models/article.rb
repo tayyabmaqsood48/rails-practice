7511e599104585b1019ec3a396d02fbd4a8588e0
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-07-14 20:24:39 +0500

    Secound Model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
   # input validations.
+  has_many :comments
+  # Comment added after 2nd class
+  # by adding above line we can retreive all comments against the article by @article.comments in an array
   validates :title, presence: true
-  validates :body, presence:true, length: {minimum: 10}
+  validates :body, presence: true, length: { minimum: 10 }
 end
