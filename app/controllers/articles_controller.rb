5856b8daf943fbdae076b90ee1cf8e72c90da65d
tayyabmaqsood48 <tayyab.maqsood@devsinc.com>
2021-07-14 15:38:33 +0500

    Read Operation of CRUD

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
+# frozen_string_litral:true
 class ArticlesController < ApplicationController
   def index
+    # This will fetch all data from database
+    # we will use same name that we create new in our console to commit changes in database
+    # like Article.new(------------)
+    @articles = Article.all
+  end
+
+  def show
+    @articles = Article.find(params[:id])
   end
 end
